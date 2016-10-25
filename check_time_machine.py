8a725611fa7abeca12180202acb28e9ff1f693fd
micxer <micxer@micxer.de>
2016-10-26 01:16:03 +0200

    Create snapshot before changing any data and revert to that state afterwards

--- check_time_machine.py
+++ check_time_machine.py
@@ -37,3 +37,63 @@ then
     sed -ibck 's/<key>RecoveryBackupDeclinedDate</key>//g' "$TM_BUNDLE/com.apple.TimeMachine.MachineID.plist"
 fi
 """
+
+from os import path
+import argparse
+import yaml
+from paramiko.client import SSHClient
+
+class TimeMachineFixer(object):
+  initial_snapshot = None
+  current_snapshot = None
+  ssh_connection = None
+  configuration = None
+
+  def __init__(self, configuration):
+    self.configuration = configuration
+    self.ssh_connection = SSHClient()
+    self.ssh_connection.load_system_host_keys()
+    self.ssh_connection.connect(self.configuration['freenas_host'])
+
+  def create_initial_snapshot(self):
+    """
+    Create snapshot before changing any data
+    """
+    self.initial_snapshot = self.configuration['dataset'] + '@time-machine-fixer'
+    stdin, stdout, stderr = self.ssh_connection.exec_command('sudo zfs snapshot -r ' + self.initial_snapshot)
+    for line in stdout.readlines():
+      print line.strip()
+    for line in stderr.readlines():
+      print line.strip()
+    stdin, stdout, stderr = self.ssh_connection.exec_command('ls -1 /mnt/' + self.configuration['dataset'] + '/.zfs/snapshot/')
+    for line in stdout.readlines():
+      print line.strip()
+
+  def destroy_initial_snapshot(self):
+    """
+    Go back to the state before start of fixing process
+    """
+    stdin, stdout, stderr = self.ssh_connection.exec_command('sudo zfs destroy -r ' + self.initial_snapshot)
+    for line in stdout.readlines():
+      print line.strip()
+    stdin, stdout, stderr = self.ssh_connection.exec_command('ls -1 /mnt/' + self.configuration['dataset'] + '/.zfs/snapshot/')
+    for line in stdout.readlines():
+      print line.strip()
+
+
+def load_configuration(config_file):
+  config_stream = open(config_file, "r")
+  configuration = yaml.load(config_stream)
+  return configuration
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("sparsebundle")
+  args = parser.parse_args()
+  configuration = load_configuration(path.expanduser("~") + "/.time-machine-fixer.yml")
+  tmf = TimeMachineFixer(configuration)
+  tmf.create_initial_snapshot()
+  tmf.destroy_initial_snapshot()
+
+if __name__ == '__main__':
+  main()
