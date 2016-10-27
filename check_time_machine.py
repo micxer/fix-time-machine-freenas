cfe62977d7ee3318f5827ee3927fc7ec35720e83
micxer <micxer@micxer.de>
2016-10-28 01:12:32 +0200

    Get list of existing snapshots

--- check_time_machine.py
+++ check_time_machine.py
@@ -46,79 +46,88 @@ from os import path
 from paramiko.client import SSHClient
 
 class TimeMachineFixer(object):
-  initial_snapshot = None
-  current_snapshot = None
-  ssh_connection = None
-  configuration = None
+    current_datetime = None
+    initial_snapshot = None
+    current_snapshot = None
+    snapshot_list = {}
+    ssh_connection = None
+    configuration = None
 
-  def __init__(self, configuration, logger):
-    self.configuration = configuration
-    self.ssh_connection = SSHClient()
-    self.ssh_connection.load_system_host_keys()
-    self.ssh_connection.connect(self.configuration['freenas_host'])
-    self.logger = logger
+    def __init__(self, configuration, logger):
+        self.configuration = configuration
+        self.ssh_connection = SSHClient()
+        self.ssh_connection.load_system_host_keys()
+        self.ssh_connection.connect(self.configuration['freenas_host'])
+        self.logger = logger
+        self.current_datetime = datetime.now()
 
-  def create_initial_snapshot(self):
-    """
-    Create snapshot before changing any data
-    """
-    self.initial_snapshot = self.configuration['dataset'] + '@time-machine-fixer-' + datetime.now().strftime('%Y%m%d-%H%M%S')
-    self.logger.info('Initial snapshot name: %s', self.initial_snapshot)
+    def create_initial_snapshot(self):
+        """
+        Create snapshot before changing any data
+        """
+        self.initial_snapshot = self.configuration['dataset'] + '@time-machine-fixer-' + self.current_datetime.strftime('%Y%m%d-%H%M%S')
+        self.logger.info('Initial snapshot name: %s', self.initial_snapshot)
 
-    create_snapshot_cmd = 'sudo zfs snapshot -r ' + self.initial_snapshot
-    self.logger.debug('Create snapshot command: %s', create_snapshot_cmd)
-    stdin, stdout, stderr = self.ssh_connection.exec_command(create_snapshot_cmd)
-    self.logger.error("".join(stderr.readlines()))
-    self.logger.info("".join(stdout.readlines()))
+        create_snapshot_cmd = 'sudo zfs snapshot -r ' + self.initial_snapshot
+        self.logger.debug('Create snapshot command: %s', create_snapshot_cmd)
+        stdin, stdout, stderr = self.ssh_connection.exec_command(create_snapshot_cmd)
+        self.logger.error("".join(stderr.readlines()))
+        self.logger.info("".join(stdout.readlines()))
 
-    list_snapshots_cmd = 'ls -1 /mnt/' + self.configuration['dataset'] + '/.zfs/snapshot/'
-    self.logger.debug('List snapshots command: %s', list_snapshots_cmd)
-    stdin, stdout, stderr = self.ssh_connection.exec_command(list_snapshots_cmd)
-    self.logger.debug(",".join([x.strip() for x in stdout.readlines()]))
-    
+    def destroy_initial_snapshot(self):
+        """
+        Go back to the state before start of fixing process
+        """
+        destroy_snapshot_cmd = 'sudo zfs destroy -r ' + self.initial_snapshot
+        self.logger.debug('Destroy snapshot command: %s', destroy_snapshot_cmd)
+        stdin, stdout, stderr = self.ssh_connection.exec_command(destroy_snapshot_cmd)
+        self.logger.error("".join(stderr.readlines()))
+        self.logger.info("".join(stdout.readlines()))
 
-  def destroy_initial_snapshot(self):
-    """
-    Go back to the state before start of fixing process
-    """
-    destroy_snapshot_cmd = 'sudo zfs destroy -r ' + self.initial_snapshot
-    self.logger.debug('Destroy snapshot command: %s', destroy_snapshot_cmd)
-    stdin, stdout, stderr = self.ssh_connection.exec_command(destroy_snapshot_cmd)
-    self.logger.error("".join(stderr.readlines()))
-    self.logger.info("".join(stdout.readlines()))
+    def get_snapshot_list(self):
+        """
+        Gets a list of snapshots from the dataset on FreeNAS
+        """
+        list_snapshots_cmd = 'ls -1 /mnt/' + self.configuration['dataset'] + '/.zfs/snapshot/'
+        self.logger.debug('List snapshots command: %s', list_snapshots_cmd)
+        stdin, stdout, stderr = self.ssh_connection.exec_command(list_snapshots_cmd)
+        snapshot_list = [snapshot.strip() for snapshot in stdout.readlines() if snapshot.startswith('auto-')]
+        self.logger.debug(",".join(snapshot_list))
+        # auto-20160827.0003-2m
+        for snapshot in snapshot_list:
+            self.snapshot_list[datetime.strptime(snapshot.split('-')[1], '%Y%m%d.%H%M')] = snapshot
+        self.logger.debug(self.snapshot_list)
 
-    list_snapshots_cmd = 'ls -1 /mnt/' + self.configuration['dataset'] + '/.zfs/snapshot/'
-    self.logger.debug('List snapshots command: %s', list_snapshots_cmd)
-    stdin, stdout, stderr = self.ssh_connection.exec_command(list_snapshots_cmd)
-    self.logger.debug(",".join([x.strip() for x in stdout.readlines()]))
 
 
 def load_configuration(config_file):
-  config_stream = open(config_file, "r")
-  configuration = yaml.load(config_stream)
-  return configuration
+    config_stream = open(config_file, "r")
+    configuration = yaml.load(config_stream)
+    return configuration
 
 def setup_logger(loglevel):
-	numeric_level = getattr(logging, loglevel.upper(), None)
-	if not isinstance(numeric_level, int):
-		raise ValueError('Invalid log level: %s' % loglevel)
-	logging.basicConfig(filename='fix-time-machine.log',format='%(levelname)s: %(asctime)s:%(message)s', datefmt='%b %d %H:%M:%S', level=numeric_level)
-	return logging.getLogger(__name__)
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+    logging.basicConfig(filename='fix-time-machine.log',format='%(levelname)s: %(asctime)s:%(message)s', datefmt='%b %d %H:%M:%S', level=numeric_level)
+    return logging.getLogger(__name__)
 
 def main():
-  parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-  parser.add_argument("sparsebundle", help='Name of the sparsebundle to fix (without the extension, e.g. "MyMacBook" for MyMacBook.sparsebundle)')
-  parser.add_argument("--loglevel", help='Level of logging (CRITICAL, ERROR, WARNING (default), INFO, DEBUG)', default="WARNING")
+    parser.add_argument("sparsebundle", help='Name of the sparsebundle to fix (without the extension, e.g. "MyMacBook" for MyMacBook.sparsebundle)')
+    parser.add_argument("--loglevel", help='Level of logging (CRITICAL, ERROR, WARNING (default), INFO, DEBUG)', default="WARNING")
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  logger = setup_logger(args.loglevel)
-  configuration = load_configuration(path.expanduser("~") + "/.time-machine-fixer.yml")
+    logger = setup_logger(args.loglevel)
+    configuration = load_configuration(path.expanduser("~") + "/.time-machine-fixer.yml")
 
-  tmf = TimeMachineFixer(configuration, logger)
-  tmf.create_initial_snapshot()
-  tmf.destroy_initial_snapshot()
+    tmf = TimeMachineFixer(configuration, logger)
+    tmf.create_initial_snapshot()
+    tmf.get_snapshot_list()
+    tmf.destroy_initial_snapshot()
+    tmf.get_snapshot_list()
 
 if __name__ == '__main__':
-  main()
+    main()
