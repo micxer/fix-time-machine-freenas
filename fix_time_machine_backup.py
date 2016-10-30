41309562c1da7c540528a7f2e68a18518ccc77ae
micxer <micxer@micxer.de>
2016-10-30 23:22:28 +0100

    Add checking and revert logic

--- fix_time_machine_backup.py
+++ fix_time_machine_backup.py
@@ -13,15 +13,17 @@ import yaml
 from datetime import datetime
 from os import path
 from paramiko.client import SSHClient
+import subprocess
 
 class TimeMachineFixer(object):
 
-    def __init__(self, configuration, logger):
+    def __init__(self, configuration, logger, sparsebundle):
         self.__configuration = configuration
         self.__ssh_connection = SSHClient()
         self.__ssh_connection.load_system_host_keys()
         self.__ssh_connection.connect(self.__configuration['freenas_host'])
         self.logger = logger
+        self.__sparsebundle = sparsebundle
         self.__current_datetime = datetime.now()
 
     def create_rollback_snapshot(self):
@@ -71,6 +73,64 @@ class TimeMachineFixer(object):
         self.logger.debug(snapshot_dict)
         return snapshot_dict
 
+    def revert_to_snapshot(self, snapshot):
+        cmd = 'rsync -avPh --delete /{mount_prefix}/{dataset}/.zfs/snapshot/{snapshot}/{sparsebundle}.sparsebundle/ /{mount_prefix}/{dataset}/{sparsebundle}.sparsebundle/'.format(
+            mount_prefix=self.__configuration['mount_prefix'],
+            dataset=self.__configuration['dataset'],
+            snapshot=snapshot,
+            sparsebundle=self.__sparsebundle
+        )
+        self.logger.info('FreeNAS: %s', cmd)
+        stdin, stdout, stderr = self.__ssh_connection.exec_command(cmd)
+        self.logger.error("".join(stderr.readlines()))
+        self.logger.debug("".join(stdout.readlines()))
+
+    def fsck_sparsebundle(self):
+        self._prepare_sparsebundle()
+        self._mount_sparsebundle()
+        self._do_fsck()
+        self._unmount_sparsebundle()
+
+    def _mount_sparsebundle(self):
+        cmd = 'sudo hdiutil attach -nomount -noverify -noautofsck /Volumes/Time\ Machine/' + self.__sparsebundle + '.sparsebundle'
+        self.logger.info(cmd)
+        labels = subprocess.check_output(cmd, shell=True)
+        self.logger.debug(labels)
+        self.__disk = labels.split()[-2]
+
+    def _unmount_sparsebundle(self):
+        cmd = 'sudo hdiutil detach ' + self.__disk
+        self.logger.info(cmd)
+        output = subprocess.check_output(cmd, shell=True)
+        self.logger.debug(output)
+        self.__disk = None
+
+    def _prepare_sparsebundle(self):
+        sparsebundle_path = '/Volumes/Time Machine/' + self.__sparsebundle + '.sparsebundle'
+        cmd = 'sudo chflags nouchg "{0}" "{0}/token"'.format(sparsebundle_path)
+        self.logger.info(cmd)
+        output = subprocess.check_output(cmd, shell=True)
+        self.logger.debug(output)
+
+    def _do_fsck(self):
+        fsck_full_check_cmd = 'sudo time fsck_hfs -dfry -c 1g ' + self.__disk
+        fsck_prune_cmd = 'sudo time fsck_hfs -p ' + self.__disk
+        try:
+            self.logger.info(fsck_full_check_cmd)
+            output = subprocess.check_output(fsck_full_check_cmd, shell=True)
+            self.logger.debug(output)
+        except subprocess.CalledProcessError as e:
+            self.logger.warning(e.output)
+            try:
+                self.logger.info(fsck_prune_cmd)
+                output = subprocess.check_output(fsck_prune_cmd, shell=True)
+                self.logger.info(fsck_full_check_cmd)
+                output = subprocess.check_output(fsck_full_check_cmd, shell=True)
+            except subprocess.CalledProcessError as e:
+                self.logger.error(e.output)
+                return False
+
+        return True
 
 def load_configuration(config_file):
     config_stream = open(config_file, "r")
@@ -95,9 +155,17 @@ def main():
     logger = setup_logger(args.loglevel)
     configuration = load_configuration(path.expanduser("~") + "/.time-machine-fixer.yml")
 
-    tmf = TimeMachineFixer(configuration, logger)
+    tmf = TimeMachineFixer(configuration, logger, args.sparsebundle)
     tmf.create_rollback_snapshot()
     snapshots = tmf.get_snapshot_list()
+    for timestamp, snapshot in snapshots.iteritems():
+        try:
+            tmf.revert_to_snapshot(snapshot)
+            tmf.fsck_sparsebundle()
+        except Exception:
+            continue
+        print 'Snapshot {0} contains a working sparsebundle'.format(snapshot)
+        break
 #    tmf.destroy_rollback_snapshot()
 #    tmf.get_snapshot_list()
 
